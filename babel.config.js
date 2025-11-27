5b0b7542674d1874b00c81633a0123e7ff217640
valentina <valentinacalvod1@upb.edu>
2025-11-27 01:22:46 -0400

    test: add minimal jest config and sanity test for CI

--- babel.config.js
+++ babel.config.js
@@ -19,7 +19,7 @@ module.exports = function (api) {
           extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"],
         },
       ],
-      "react-native-reanimated/plugin",
+      'react-native-reanimated/plugin',
     ],
   };
 };
